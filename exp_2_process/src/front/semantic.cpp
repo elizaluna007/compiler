2e7e670b1e6541ca28c067a71fdf562edf4dbe68
elizaluna007 <2194761865@qq.com>
2023-04-30 14:44:21 +0800

    update

--- exp_2_process/src/front/semantic.cpp
+++ exp_2_process/src/front/semantic.cpp
@@ -1,20 +1,29 @@
-#include"front/semantic.h"
+#include "front/semantic.h"
 
-#include<cassert>
+#include <cassert>
 
-using ir::Instruction;
 using ir::Function;
+using ir::Instruction;
 using ir::Operand;
 using ir::Operator;
 
 #define TODO assert(0 && "TODO");
 
-#define GET_CHILD_PTR(node, type, index) auto node = dynamic_cast<type*>(root->children[index]); assert(node); 
-#define ANALYSIS(node, type, index) auto node = dynamic_cast<type*>(root->children[index]); assert(node); analysis##type(node, buffer);
-#define COPY_EXP_NODE(from, to) to->is_computable = from->is_computable; to->v = from->v; to->t = from->t;
+#define GET_CHILD_PTR(node, type, index)                     \
+    auto node = dynamic_cast<type *>(root->children[index]); \
+    assert(node);
+#define ANALYSIS(node, type, index)                          \
+    auto node = dynamic_cast<type *>(root->children[index]); \
+    assert(node);                                            \
+    analysis##type(node, buffer);
+#define COPY_EXP_NODE(from, to)              \
+    to->is_computable = from->is_computable; \
+    to->v = from->v;                         \
+    to->t = from->t;
 
-map<std::string,ir::Function*>* frontend::get_lib_funcs() {
-    static map<std::string,ir::Function*> lib_funcs = {
+map<std::string, ir::Function *> *frontend::get_lib_funcs()
+{
+    static map<std::string, ir::Function *> lib_funcs = {
         {"getint", new Function("getint", Type::Int)},
         {"getch", new Function("getch", Type::Int)},
         {"getfloat", new Function("getfloat", Type::Float)},
@@ -29,29 +38,36 @@ map<std::string,ir::Function*>* frontend::get_lib_funcs() {
     return &lib_funcs;
 }
 
-void frontend::SymbolTable::add_scope(Block* node) {
-    TODO;
+void frontend::SymbolTable::add_scope(Block *node)
+{
+    // std::cout << "add scope" << std::endl;
 }
-void frontend::SymbolTable::exit_scope() {
-    TODO;
-}
-
-string frontend::SymbolTable::get_scoped_name(string id) const {
-    TODO;
+void frontend::SymbolTable::exit_scope()
+{
+    // std::cout << "exit scope" << std::endl;
 }
 
-Operand frontend::SymbolTable::get_operand(string id) const {
-    TODO;
+string frontend::SymbolTable::get_scoped_name(string id) const
+{
+    std::cout << "get scoped name" << std::endl;
 }
 
-frontend::STE frontend::SymbolTable::get_ste(string id) const {
-    TODO;
+Operand frontend::SymbolTable::get_operand(string id) const
+{
+    std::cout << "get operand" << std::endl;
 }
 
-frontend::Analyzer::Analyzer(): tmp_cnt(0), symbol_table() {
-    TODO;
+frontend::STE frontend::SymbolTable::get_ste(string id) const
+{
+    std::cout << "get ste" << std::endl;
 }
 
-ir::Program frontend::Analyzer::get_ir_program(CompUnit* root) {
-    TODO;
+frontend::Analyzer::Analyzer() : tmp_cnt(0), symbol_table()
+{
+    std::cout << "analyzer" << std::endl;
+}
+
+ir::Program frontend::Analyzer::get_ir_program(CompUnit *root)
+{
+    std::cout << "get ir program" << std::endl;
 }

3cd4d5455c0d3a543e9115138bead067906a5696
elizaluna007 <2194761865@qq.com>
2023-05-20 00:18:13 +0800

    s3更新框架

--- exp_3/include/backend/generator.h
+++ exp_3/include/backend/generator.h
@@ -5,55 +5,95 @@
 #include "backend/rv_def.h"
 #include "backend/rv_inst_impl.h"
 
-#include<map>
-#include<string>
-#include<vector>
-#include<fstream>
+#include <map>
+#include <string>
+#include <vector>
+#include <fstream>
 
-namespace backend {
+namespace backend
+{
 
-// it is a map bewteen variable and its mem addr, the mem addr of a local variable can be identified by ($sp + off)
-struct stackVarMap {
-    std::map<ir::Operand, int> _table;
+    // it is a map bewteen variable and its mem addr, the mem addr of a local variable can be identified by ($sp + off)
+    struct stackVarMap
+    {
+        std::map<ir::Operand, int> _table;
 
-    /**
-     * @brief find the addr of a ir::Operand
-     * @return the offset
-    */
-    int find_operand(ir::Operand);
+        /**
+         * @brief find the addr of a ir::Operand
+         * @return the offset
+         */
+        int find_operand(ir::Operand);
 
-    /**
-     * @brief add a ir::Operand into current map, alloc space for this variable in memory 
-     * @param[in] size: the space needed(in byte)
-     * @return the offset
-    */
-    int add_operand(ir::Operand, uint32_t size = 4);
-};
+        /**
+         * @brief add a ir::Operand into current map, alloc space for this variable in memory
+         * @param[in] size: the space needed(in byte)
+         * @return the offset
+         */
+        int add_operand(ir::Operand, uint32_t size = 4);
+    };
 
+    struct Generator
+    {
+        const ir::Program &program; // the program to gen
+        std::ofstream &fout;        // output file
 
-struct Generator {
-    const ir::Program& program;         // the program to gen
-    std::ofstream& fout;                 // output file
+        Generator(ir::Program &, std::ofstream &);
 
-    Generator(ir::Program&, std::ofstream&);
+        // reg allocate api
+        rv::rvREG getRd(ir::Operand);
+        rv::rvFREG fgetRd(ir::Operand);
+        rv::rvREG getRs1(ir::Operand);
+        rv::rvREG getRs2(ir::Operand);
+        rv::rvFREG fgetRs1(ir::Operand);
+        rv::rvFREG fgetRs2(ir::Operand);
 
-    // reg allocate api
-    rv::rvREG getRd(ir::Operand);
-    rv::rvFREG fgetRd(ir::Operand);
-    rv::rvREG getRs1(ir::Operand);
-    rv::rvREG getRs2(ir::Operand);
-    rv::rvFREG fgetRs1(ir::Operand);
-    rv::rvFREG fgetRs2(ir::Operand);
+        // generate wrapper function
+        void gen();
+        void gen_func(const ir::Function &);
+        void gen_instr(const ir::Instruction &);
 
-    // generate wrapper function
-    void gen();
-    void gen_func(const ir::Function&);
-    void gen_instr(const ir::Instruction&);
-};
-
-
+        void gen_return(const ir::Instruction &);
+        void gen_goto(const ir::Instruction &);
+        void gencall(const ir::Instruction &);
+        void genalloc(const ir::Instruction &);
+        void genstore(const ir::Instruction &);
+        void genload(const ir::Instruction &);
+        void gengetptr(const ir::Instruction &);
+        void gendef(const ir::Instruction &);
+        void genfdef(const ir::Instruction &);
+        void genmov(const ir::Instruction &);
+        void genfmov(const ir::Instruction &);
+        void gencvt_i2f(const ir::Instruction &);
+        void gencvt_f2i(const ir::Instruction &);
+        void genadd(const ir::Instruction &);
+        void genaddi(const ir::Instruction &);
+        void genfadd(const ir::Instruction &);
+        void gensub(const ir::Instruction &);
+        void gensubi(const ir::Instruction &);
+        void genfsub(const ir::Instruction &);
+        void genmul(const ir::Instruction &);
+        void genfmul(const ir::Instruction &);
+        void gendiv(const ir::Instruction &);
+        void genfdiv(const ir::Instruction &);
+        void genmod(const ir::Instruction &);
+        void genlss(const ir::Instruction &);
+        void genflss(const ir::Instruction &);
+        void genleq(const ir::Instruction &);
+        void genfleq(const ir::Instruction &);
+        void gengtr(const ir::Instruction &);
+        void genfgtr(const ir::Instruction &);
+        void gengeq(const ir::Instruction &);
+        void genfgeq(const ir::Instruction &);
+        void geneq(const ir::Instruction &);
+        void genfeq(const ir::Instruction &);
+        void genneq(const ir::Instruction &);
+        void genfneq(const ir::Instruction &);
+        void gen_not(const ir::Instruction &);
+        void gen_and(const ir::Instruction &);
+        void gen_or(const ir::Instruction &);
+        void gen__unuse__(const ir::Instruction &);
+    };
 
 } // namespace backend
-
 
 #endif
--- exp_3/src/backend/generator.cpp
+++ exp_3/src/backend/generator.cpp
@@ -1,12 +1,274 @@
-#include"backend/generator.h"
+#include "backend/generator.h"
 
-#include<assert.h>
-
+#include <assert.h>
 
 #define TODO assert(0 && "todo")
 
-backend::Generator::Generator(ir::Program& p, std::ofstream& f): program(p), fout(f) {}
+backend::Generator::Generator(ir::Program &p, std::ofstream &f) : program(p), fout(f) {}
 
-void backend::Generator::gen() {
-    TODO;
+void backend::Generator::gen()
+{
+    // 遍历所有函数
+    for (auto &function : program.functions)
+    {
+        gen_func(function);
+    }
+}
+void backend::Generator::gen_func(const ir::Function &function)
+{
+    // 遍历所有指令
+    for (auto &instrPtr : function.InstVec)
+    {
+        ir::Instruction &instruction = *instrPtr;
+        gen_instr(instruction);
+    }
+}
+void backend::Generator::gen_instr(const ir::Instruction &instruction)
+{
+    switch (instruction.op)
+    {
+    case ir::Operator::_return:
+        gen_return(instruction);
+        break;
+    case ir::Operator::_goto:
+        gen_goto(instruction);
+        break;
+    case ir::Operator::call:
+        gencall(instruction);
+        break;
+    case ir::Operator::alloc:
+        genalloc(instruction);
+        break;
+    case ir::Operator::store:
+        genstore(instruction);
+        break;
+    case ir::Operator::load:
+        genload(instruction);
+        break;
+    case ir::Operator::getptr:
+        gengetptr(instruction);
+        break;
+    case ir::Operator::def:
+        gendef(instruction);
+        break;
+    case ir::Operator::fdef:
+        genfdef(instruction);
+        break;
+    case ir::Operator::mov:
+        genmov(instruction);
+        break;
+    case ir::Operator::fmov:
+        genfmov(instruction);
+        break;
+    case ir::Operator::cvt_i2f:
+        gencvt_i2f(instruction);
+        break;
+    case ir::Operator::cvt_f2i:
+        gencvt_f2i(instruction);
+        break;
+    case ir::Operator::add:
+        genadd(instruction);
+        break;
+    case ir::Operator::addi:
+        genaddi(instruction);
+        break;
+    case ir::Operator::fadd:
+        genfadd(instruction);
+        break;
+    case ir::Operator::sub:
+        gensub(instruction);
+        break;
+    case ir::Operator::subi:
+        gensubi(instruction);
+        break;
+    case ir::Operator::fsub:
+        genfsub(instruction);
+        break;
+    case ir::Operator::mul:
+        genmul(instruction);
+        break;
+    case ir::Operator::fmul:
+        genfmul(instruction);
+        break;
+    case ir::Operator::div:
+        gendiv(instruction);
+        break;
+    case ir::Operator::fdiv:
+        genfdiv(instruction);
+        break;
+    case ir::Operator::mod:
+        genmod(instruction);
+        break;
+    case ir::Operator::lss:
+        genlss(instruction);
+        break;
+    case ir::Operator::flss:
+        genflss(instruction);
+        break;
+    case ir::Operator::leq:
+        genleq(instruction);
+        break;
+    case ir::Operator::fleq:
+        genfleq(instruction);
+        break;
+    case ir::Operator::gtr:
+        gengtr(instruction);
+        break;
+    case ir::Operator::fgtr:
+        genfgtr(instruction);
+        break;
+    case ir::Operator::geq:
+        gengeq(instruction);
+        break;
+    case ir::Operator::fgeq:
+        genfgeq(instruction);
+        break;
+    case ir::Operator::eq:
+        geneq(instruction);
+        break;
+    case ir::Operator::feq:
+        genfeq(instruction);
+        break;
+    case ir::Operator::neq:
+        genneq(instruction);
+        break;
+    case ir::Operator::fneq:
+        genfneq(instruction);
+        break;
+    case ir::Operator::_not:
+        gen_not(instruction);
+        break;
+    case ir::Operator::_and:
+        gen_and(instruction);
+        break;
+    case ir::Operator::_or:
+        gen_or(instruction);
+        break;
+    case ir::Operator::__unuse__:
+        gen__unuse__(instruction);
+        break;
+
+    default:
+        break;
+    }
+}
+void backend::Generator::gen_return(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gen_goto(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gencall(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genalloc(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genstore(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genload(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gengetptr(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gendef(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfdef(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genmov(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfmov(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gencvt_i2f(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gencvt_f2i(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genadd(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genaddi(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfadd(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gensub(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gensubi(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfsub(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genmul(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfmul(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gendiv(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfdiv(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genmod(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genlss(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genflss(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genleq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfleq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gengtr(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfgtr(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gengeq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfgeq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::geneq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfeq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genneq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::genfneq(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gen_not(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gen_and(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gen_or(const ir::Instruction &instruction)
+{
+}
+void backend::Generator::gen__unuse__(const ir::Instruction &instruction)
+{
 }
